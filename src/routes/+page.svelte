effb7508dbb80265793d57fbfde1f2372cc9dee2
Ludvig Svedberg <ludvig.svedberg@mac.com>
2022-09-05 16:43:42 +0200

    Remove demo code and add simple content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,20 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>D Styr</title>
+	<meta name="description" content="D som styr D-sektionen" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div class="container">
+		<h1 class="title">Vem styr? <a href="/">D Styr</a>!</h1>
+	</div>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.title {
+		text-align: center;
+		font-size: 4rem;
 	}
 </style>
